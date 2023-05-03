d72d3656d208150bc3c943964a100a0f735c0d0f
leezer94 <2kunhee94@gmail.com>
2023-05-03 18:46:16 +0900

    cors options 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000/',
+    preflightContinue: false,
+  });
   // const reflector = new Reflector();
   // app.useGlobalGuards(new JwtGuard(reflector)); // auth.module.ts providers
   await app.listen(8000);
