d0e505358b4c4f165777ff977526c795328d49e6
keonhee Lee <2kunhee94@gmail.com>
2023-05-19 20:19:42 +0900

    feat : add rss service ;

---
+++ server/src/rss/rss.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RssService } from './rss.service';
+import { RssController } from './rss.controller';
+
+@Module({
+  providers: [RssService],
+  controllers: [RssController]
+})
+export class RssModule {}
