350594234b7e3c28a6afa11b710f10d27fb8a127
keonhee Lee <2kunhee94@gmail.com>
2023-05-19 16:50:10 +0900

    chore : add dependency tailwind-scrollbar-hide

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,7 +12,11 @@ module.exports = {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1400px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
     },
     extend: {
@@ -72,5 +76,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar-hide')],
 };
